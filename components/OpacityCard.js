52001c28f6b95bc19c3378bd46024d50210ba1a9
Ivonne <ivonne.scarlett01@gmail.com>
2022-03-31 16:31:15 -0600

    Se agrego padding left y right de 20 px, y padding top y botton de 16px

--- components/OpacityCard.js
+++ components/OpacityCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function OpacityCard({children}) {
   return (
-    <div className='bg-backgroundP bg-opacity-80 rounded-xl w-80 h-64'>
+    <div className='bg-backgroundP bg-opacity-80 rounded-xl py-4 px-5 w-80 h-64'>
   
     </div>
   )
