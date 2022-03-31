416e356183c09fe29ee898a4ad19cee9280d09ab
Ivonne <ivonne.scarlett01@gmail.com>
2022-03-31 16:23:39 -0600

    Se agrego la prop children, se eliminaron las comillas dobles y el espacio al final del className

--- components/OpacityCard.js
+++ components/OpacityCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function OpacityCard() {
+export default function OpacityCard({children}) {
   return (
-    <div className="bg-backgroundP bg-opacity-80 rounded-xl w-80 h-64" >
-        
+    <div className='bg-backgroundP bg-opacity-80 rounded-xl w-80 h-64'>
+  
     </div>
   )
 }
