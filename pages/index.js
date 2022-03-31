4cca955a6d18ff68e163b46f739aec6ee74c32d3
Ivonne <ivonne.scarlett01@gmail.com>
2022-03-31 16:51:02 -0600

    Se agrego componente OpacityCrad para prueba en index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import OpacityCard from '../components/OpacityCard'
 
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className="bg-pink-500">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        
-        
-       
-      </main>
-
-      <footer className={styles.footer}>
-        
-      </footer>
+    <div>
+      <h1 className="bg-pink-500">
+        Welcome to Mappearte
+      </h1>        
+      <OpacityCard>
+      </OpacityCard>
     </div>
   )
 }
