7a43e2ddefc212248a60bc8749360d04adf0cfc5
Ivonne <ivonne.scarlett01@gmail.com>
2022-03-31 16:50:10 -0600

    Se agrego prueba de texto al componente

--- components/OpacityCard.js
+++ components/OpacityCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function OpacityCard({children}) {
   return (
     <div className='bg-backgroundP bg-opacity-80 rounded-xl py-4 px-5 w-80 h-64'>
-  
+        <p className='text-white'>Texto de prueba dentro de la OpacityCard</p>
     </div>
   )
 }
