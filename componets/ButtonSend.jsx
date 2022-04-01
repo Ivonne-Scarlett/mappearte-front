87dc2758cf14e8bb29266f7cc0f84f0493514123
Sara Reveles <elizabethrc@gmail.com>
2022-03-31 21:43:17 -0400

    Componente de botones sin funcion

---
+++ componets/ButtonSend.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ButtonSend({color, nameButton, onClick}) {
+  const colorConfig = {
+      orange: "bg-orangeP hover:border-orangeP",
+      pink: "bg-rosaP hover:border-rosaP",
+      aqua: "bg-aquaP hover:border-aquaP",
+    };
+    return (
+    <button 
+    className={`${colorConfig[color]} bg-aquaP border-2 border-aquaP 
+    rounded font-semibold  
+    opacity-6 px-2 py-2 w-auto text-sm`} 
+    onClick={onClick}
+    >
+      {nameButton}
+    </button>
+  )
+}
