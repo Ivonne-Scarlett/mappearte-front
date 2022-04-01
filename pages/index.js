20a9e6cb4457fc2bc6b5039b35a3642726b3f630
Ivonne <ivonne.scarlett01@gmail.com>
2022-03-31 19:20:36 -0600

    Se elimino de index.js el contenido anterior

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import Head from 'next/head'
-import Image from 'next/image'
-import OpacityCard from '../components/OpacityCard'
-
-
-export default function Home() {
-  return (
-    <div>
-     
-=======
 
 
 
@@ -17,7 +6,6 @@ export default function Home() {
     <div>
               
 
->>>>>>> develop
     </div>
   )
 }
