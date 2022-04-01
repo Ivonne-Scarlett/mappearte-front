55d3ba606fcf2459419c6411e5cf7fe827244284
Ivonne <ivonne.scarlett01@gmail.com>
2022-03-31 23:09:11 -0600

    Pruebas de tipos de fuentes, se definieron Mali, MochiyPopOne, Alata

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,9 @@ import OpacityCard from '../components/OpacityCard'
 
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-test'>
-      <OpacityCard className='w-1/2 mt-8'>
-        <h1 className='font-[Gabriela] text-3xl'>Hola</h1>
-      </OpacityCard>
-      <OpacityCard className='w-1/2 mt-8'>
-        <h1 className='font-[Megrim] text-3xl'>SUBE TU ARTE</h1>
+    <div className='w-full h-screen bg-test '>
+      <OpacityCard className='w-1/2 mt-5'>
+        <h1 className='font-[Mali] text-4xl mt-3'>Artistas más populares 1</h1> <span className='text-base font-semibold font-[]'>Alicia</span>
         <p className='text-white mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia maiores molestiae voluptatibus aliquam vel alias facilis blanditiis dolorum odit molestias ullam animi eos nulla, saepe tenetur voluptate vitae? Odit, ipsum.
         Temporibus labore reiciendis voluptate incidunt, assumenda rem? Aliquam, cumque cum neque provident quia temporibus inventore voluptatibus molestiae quas quis doloremque magni dolorum ab iure aut corporis blanditiis nulla sint ex!</p>
       </OpacityCard>
