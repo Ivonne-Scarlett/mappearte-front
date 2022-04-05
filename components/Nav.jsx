a51a94d3ec23abcbcbfa58e5c58636382b53d9d3
Sara Reveles <elizabethrc@gmail.com>
2022-04-05 12:05:16 -0400

    Se agregaron imagenes en la vista de web

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -19,8 +19,10 @@ export default function div ({children }) {
     <div className='w-full fixed top-0 left-0'>
       <div className='flex justify-between items-start  py-4 px-7 mdpx-10'>
         <div className='flex '>
-          <img src='/icons/logoMobile.png' alt="Logo" className=''/>
-          <img src='/icons/MappeArteMobile.png' alt="Mapearte" className='ml-4'/>
+          <img src='/icons/logoMobile.png' alt="logoMobile.png" className='block md:hidden justify-center items-center absolute'/>
+          <img src='/icons/MappeArteMobile.png' alt="logoMobile.png" className='block md:hidden pl-12 justify-center items-center min-w-fit h-11 '/>
+          <img src='/icons/logoDesktop.png' alt="logoMobile.png" className='hidden md:block justify-center items-center absolute'/>
+          <img src='/icons/MappeArteDesktop.png' alt="logoMobile.png" className='hidden md:block pl-16 justify-center items-center absolute min-w-fit h-16' />
         </div>   
         <div className='flex flex-col md:flex-row'>
           <button className='' onClick={() => setMenuOpen(!isMenuOpen)}>
