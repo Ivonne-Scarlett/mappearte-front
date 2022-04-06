9824de3f2d239ae0ddbc1ba47db7431041081a38
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-06 16:33:48 -0500

    Se asigno la variable sizeDivIMg para gurdar los posibles valores de classNames

--- components/GridProfile.js
+++ components/GridProfile.js
@@ -1,50 +1,57 @@
-import React from 'react'
+import React from 'react';
+import classNames from 'classnames';
 
-export default function GridProfile({children, imageProfile, imageProfile1, imageProfile2, imageProfile3}) {
+export default function GridProfile({className, children, imageProfile, imageProfile1, imageProfile2, imageProfile3}) {
+  var sizeDivImg = classNames({
+    'oneSize': 'md:col-span-2  md:row-span-2',
+    'twoSize': 'col-span-2',
+    'threeSize': 'col-span-2 md:col-span-1',
+  })
   return (
     <div className="grid grid-cols-3 md:grid-cols-5">
-      <div className="mx-1.5 my-1.5 md:col-span-2  md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile1})`}}> 
-          {children}       
+    <div className={`${sizeDivImg} first-letter:mx-1.5 my-1.5 md:col-span-2 md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`}
+        style={{backgroundImage: `url(${imageProfile1})`}}> 
+        {children}       
       </div>
-      <div className="h-36 mx-1.5 my-1.5 col-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile2})`}}> 
+      <div className={`${className} h-36 mx-1.5 my-1.5 col-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`}
+        style={{backgroundImage: `url(${imageProfile2})`}}> 
+        {children}
       </div>
-      <div className="h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile3})`}}>        
+      <div className={`${className} h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile3})`}}>        
       </div>
-      <div className=" h-36 mx-1.5 my-1.5 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>         
+      <div className={`${className} h-36 mx-1.5 my-1.5 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile})`}}>         
       </div>
-      <div className="mx-1.5 my-1.5 md:col-span-2  md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>         
+      <div className={`${className} mx-1.5 my-1.5 md:col-span-2  md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`}
+        style={{backgroundImage: `url(${imageProfile})`}}>         
       </div>
-      <div className="h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>          
+      <div className={`${className} h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile})`}}>          
       </div>
-      <div className="h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>         
+      <div className={`${className} h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile})`}}>         
       </div>
-      <div className="mx-1.5 my-1.5 md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>       
+      <div className={`${className} file:mx-1.5 my-1.5 md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`}
+        style={{backgroundImage: `url(${imageProfile})`}}>       
       </div>
-      <div className="mx-1.5 my-1.5 md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>         
+      <div className={`${className} file:mx-1.5 my-1.5 md:row-span-2 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`}
+        style={{backgroundImage: `url(${imageProfile})`}}>         
       </div>
-      <div className="h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>        
+      <div className={`${className} h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile})`}}>        
       </div>
-      <div className="h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}> 
+      <div className={`${className} h-36 mx-1.5 my-1.5 col-span-2 md:col-span-1 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile})`}}> 
       </div>
-      <div className="h-36 mx-1.5 my-1.5 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}> 
+      <div className={`${className} h-36 mx-1.5 my-1.5 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile})`}}> 
       </div>
-      <div className="h-36 mx-1.5 my-1.5 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>        
+      <div className={`${className} h-36 mx-1.5 my-1.5 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`} 
+        style={{backgroundImage: `url(${imageProfile})`}}>        
       </div>
-      <div className="h-36 mx-1.5 my-1.5 col-span-2 md:col-span-3 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink" style={{
-        backgroundImage: `url(${imageProfile})`}}>          
+      <div className={`${className} h-36 mx-1.5 my-1.5 col-span-2 md:col-span-3 bg-cover bg-no-repeat bg-center border-solid border-2 border-backgroundP hover:border-bpink`}
+        style={{backgroundImage: `url(${imageProfile})`}}>          
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import GridProfile from '../components/GridProfile'
 import OpacityCard from '../components/OpacityCard'
 
+let arrayImgProfile = [
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://visitpuertovallarta.com/uploads/11.png",
+]
+
 
 export default function Home() {
   return (
     <>
       <div>
-        <GridProfile imageProfile="https://visitpuertovallarta.com/uploads/11.png"
-        imageProfile1="https://visitpuertovallarta.com/uploads/11.png"
-        imageProfile2="https://visitpuertovallarta.com/uploads/11.png"
-        imageProfile3="https://visitpuertovallarta.com/uploads/11.png"
+        <GridProfile 
+          className=''
+          imageProfile="https://visitpuertovallarta.com/uploads/11.png"
+          imageProfile1="https://visitpuertovallarta.com/uploads/11.png"
+          imageProfile2="https://visitpuertovallarta.com/uploads/11.png"
+          imageProfile3="https://visitpuertovallarta.com/uploads/11.png"
         >
         </GridProfile>
       </div>          
