807b65555e12206381ae0462b379109af4baa4b7
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-06 22:05:42 -0500

    Se creo  el darkBlueCard para colocar contenido dentro

--- components/DarkBlueCard.js
+++ components/DarkBlueCard.js
@@ -2,11 +2,10 @@ import React from 'react'
 
 export default function DarkBlueCard({children, className}) {
   return (
-    <div className={`flex items-center justify-center ${className}`}>
-      <div className={`bg-[#363558] rounded-xl py-5 px-6 `}>
-        Prueba
+    <>
+      <div className={`bg-[#363558] rounded-xl py-5 px-6 ${className}`}>
         {children}
       </div>
-    </div>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import DarkBlueCard from '../components/DarkBlueCard'
 export default function Home() {
   return (
     <div>
-      <DarkBlueCard className='w-5-6 mt-5'>
+      <DarkBlueCard className='w-100 my-5 mx-6 md:mx-20'>
       </DarkBlueCard>     
 
     </div>
