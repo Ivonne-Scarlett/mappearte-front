d900291a3b529776c85ceaaa0c6283438d82e34b
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-12 00:11:40 -0500

    Depurando iconos de web y desktop

--- components/OpacityCard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OpacityCard({children, className}) {
-  return (
-    <div className={`bg-backgroundP bg-opacity-80 rounded-xl py-5 px-6 ${className}`}>
-      {children}
-    </div>
-  )
-}
