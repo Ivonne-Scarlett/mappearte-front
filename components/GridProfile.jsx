741c4654d2c3802b353914a5481cefa7a9e191c7
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-13 21:36:55 -0500

    Imagenes acomodadas dentro del grid en 3 columnas

--- components/GridProfile.jsx
+++ components/GridProfile.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
-import cameraPhoto from '../public/img/cameraPhoto.png'
-// Solo deberia recibir un array en lugar de todos los img
-export default function GridProfile({ className, images }) {
- 
-  // va la logica para el array siempre tenga 14 imagenes.
-  /*
-  saber de que tamaño de images 4 solo faltarian 10
-  for(i=0; i<10 ;i++){
-    images.push('mi imagen )
-  }
-  */
-  let countImages = images.length
-  if (countImages < 14){
-    for (let i=countImages;  i<=13; i++){
-      images.push(cameraPhoto)
-    }
-  } else if(countImages < 0){
 
-  }
-  console.log(countImages)
-
-
-  return (
-    <div className="grid grid-cols-3 md:grid-cols-5 gap-2 md:grid-row-5">
-      {images.map((image, index) => (
+export default function GridProfile({ images }) { 
+    return (
+    <div className={classNames(
+      "columns-2 lg:columns-3",
+      "px-3"
+      )}>
+    {images.map((item, index)=> {
+      return(
         <div className={classNames(
-          'h-36',
-          'md:h-auto',
-          {
-            'col-span-2': index === 1 || index === 2 || index === 5 || index === 6 || index === 9 || index === 10 ||index === 13,
-            'md:col-span-2 md:row-span-2': index === 0 || index === 4,
-            'md:col-span-1': index === 2 || index === 5 || index === 6 || index === 9 || index === 10,
-            'md:row-span-2': index === 7 || index === 8,
-            'md:col-span-3': index === 13,
-          }
-        )}
-          key={index}
+          "break-inside-avoid-column",        
+          "cursor-pointer",
+          "mb-3",      
+          "hover:opacity-70" 
+        )} 
+          key={index} 
+          //onClick={isImageClicked}
         >
-          <img src={image} className={classNames('w-full h-full object-cover')}/>
+          <img src={item} style={{width:'100%'}}/>
         </div>
-      ))
-      }
-    </div>
+      )
+    })}
+  </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,36 @@
-<<<<<<< HEAD
-import GridProfile from '../components/GridProfile';
-import OpacityCard from '../components/OpacityCard';
-import Footer from '../components/Footer';
-
-let arrayImgProfile = [
-  "https://visitpuertovallarta.com/uploads/11.png",
-  "https://visitpuertovallarta.com/uploads/11.png",
-  "https://visitpuertovallarta.com/uploads/11.png",
-  "https://visitpuertovallarta.com/uploads/11.png",
-  "https://mxcity.mx/wp-content/uploads/2016/09/murales-3.jpg",
-  "https://visitpuertovallarta.com/uploads/11.png",
-
-]
-
-=======
 import React from 'react';
-import DarkBlueCard from '../components/DarkBlueCard';
 import ButtonCamera from '../components/ButtonCamera';
 import ButtonEdit from '../components/ButtonEdit';
 import ButtonToGo from '../components/ButtonToGo';
 import ButtonSend from '../components/ButtonSend';
 import OpacityCard from '../components/OpacityCard';
 import Footer from '../components/Footer';
+import GridProfile from '../components/GridProfile';
+import DarkBlueCard from '../components/DarkBlueCard';
           
->>>>>>> develop
+let arrayImgProfile = [
+  "https://picsum.photos/id/237/200/300",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF0RU4LzhHoM_OQa-hO5sPRX30bdTNFHgfgA&usqp=CAU",
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBy8Gqgq7-yH1FBgqZtXvcvL10t-KXWjcbaQ&usqp=CAU",
+  "https://picsum.photos/id/237/200/300",
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://picsum.photos/id/237/200/300",
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://visitpuertovallarta.com/uploads/11.png",
+  "https://mxcity.mx/wp-content/uploads/2016/09/murales-3.jpg",
+  "https://visitpuertovallarta.com/uploads/11.png",
+]
+
+
 export default function Home() {
   const sayHi = () => {
     console.log('Hola')
   }
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <GridProfile
-          className=''
-          images={arrayImgProfile}
-        >
-        </GridProfile>
-      </div>
-
-      <div className='w-full h-screen bg-test relative '>
-        <OpacityCard className='w-1/2 mt-5'>
-          <h1 className='font-Mali text-4xl mt-3'>Artistas más populares 1</h1> <span className='text-base font-semibold font-Mochiy'>Alicia</span>
-          <p className='text-white mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia maiores molestiae voluptatibus aliquam vel alias facilis blanditiis dolorum odit molestias ullam animi eos nulla, saepe tenetur voluptate vitae? Odit, ipsum.
-            Temporibus labore reiciendis voluptate incidunt, assumenda rem? Aliquam, cumque cum neque provident quia temporibus inventore voluptatibus molestiae quas quis doloremque magni dolorum ab iure aut corporis blanditiis nulla sint ex!</p>
-        </OpacityCard>
-        <Footer />
-      </div>
-=======
       <main className='w-full bg-test bg-contain py-20 min-h-screen'>
         <OpacityCard className='w-1/2 mt-5 h-[95%] flex flex-col justify-between'>
           <h1 className='font-Mali text-4xl mt-3'>Artistas más populares 1</h1> <span className='text-base font-semibold font-Mochiy'>Alicia</span>
@@ -76,14 +59,17 @@ export default function Home() {
             />
           </div>     
         </OpacityCard>
-        <DarkBlueCard className='w-100 my-5 mx-6 md:mx-20'>
-          <label>Test</label>
-          <input/>
-        </DarkBlueCard>     
+
+        <DarkBlueCard className='mt-5'>
+          <GridProfile
+            className=''
+            images={arrayImgProfile}
+          />       
+        </DarkBlueCard>
+
       </main>  
             
       <Footer />
->>>>>>> develop
     </>
   )
 }
