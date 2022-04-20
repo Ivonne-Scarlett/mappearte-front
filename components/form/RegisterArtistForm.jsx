3556865c93da36ce21bbecfe21f793da24ded37b
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-19 21:18:09 -0500

    Lógica funcionando correctamente para guardar la categoria del artista

--- components/form/RegisterArtistForm.jsx
+++ components/form/RegisterArtistForm.jsx
@@ -13,30 +13,13 @@ export default function RegisterArtistForm() {
 
   const onSubmit = async(dataRegister) => {
     console.log(dataRegister)
-    // const artist = await registerArtist(dataRegister)
-    // console.log('artist:',artist)
-    // if (artist.ok) {
-    //   router.push('/Login')
-    // }
+    const artist = await registerArtist(dataRegister)
+    console.log('artist:',artist)
+    if (artist.ok) {
+      router.push('/Login')
+    }
    }
-
-   const validateEmail = () => {
-
-   }
-
-   if(dataRegister.email1 = dataRegister.email1){
-    <Input 
-    label='Correo electrónico:'
-    placeholder='ejemplo: demo@gmail.com'
-    name='email2'
-    register= {register("email")} 
-    required
-    id='email2'
-    />
-  } else {
-    alert("El correo electrónico debe ser el mismo ingresado en ambos campos de correo electrónico")
-  }
-  
+ 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Input 
@@ -85,72 +68,22 @@ export default function RegisterArtistForm() {
         name='lastName'
         register= {register("lastName")} 
         required
-      />
-      
+      />      
       <Input 
         label='Correo electrónico:'
         placeholder='ejemplo: demo@gmail.com'
-        name='email1'
+        name='email'
         register= {register("email")} 
         required
-        id='email1'
       /> 
-      <Input 
-        label='Correo electrónico:'
-        placeholder='ejemplo: demo@gmail.com'
-        name='email2'
-        register= {register("email")} 
-        required
-        id='email2'
-      />
-
-      {/* if({dataRegister.email1} = {dataRegister.email1}){
-        <Input 
-        label='Correo electrónico:'
-        placeholder='ejemplo: demo@gmail.com'
-        name='email2'
-        register= {register("email")} 
-        required
-        id='email2'
-        />
-      } else {
-        alert("El correo electrónico debe ser el mismo ingresado en ambos campos de correo electrónico")
-      } */}
-
-
        <Input 
         label='Contraseña:'
         type='password'
         placeholder='Contraseña'
-        name='password1'
-        register= {register("password")} 
-        required
-        id='password1'
-      />
-      <Input 
-        label='Contraseña:'
-        type='password2'
-        placeholder='Contraseña'
         name='password'
         register= {register("password")} 
         required
-        id='password2'
-      />
-{/* 
-      if({dataRegister.password1} = {dataRegister.password2}){
-        <Input 
-        label='Contraseña:'
-        type='password2'
-        placeholder='Contraseña'
-        name='password'
-        register= {register("password")} 
-        required
-        id='password2'
-      />
-      } else {
-        alert("La contraseña debe ser la misma ingresada en ambos campos de contraseña")
-      } */}
-     
+      />    
       <ButtonSend 
         type='submit' 
         text='Registrar'
