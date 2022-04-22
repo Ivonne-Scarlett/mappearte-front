8696ee9b7efee4baa59fee4c65da56d1852e0f40
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-21 20:40:43 -0500

    mostrar cards de searchArtist

--- pages/searchartist.js
+++ pages/searchartist.js
@@ -9,20 +9,20 @@ import OpacityCard from '../components/OpacityCard';
 import Footer from '../components/Footer';
 
 const [searchArtist, setSearchArtist] =  useState([]);
-const [inputValue, setInputValue] =  useState([]);
+// const [inputValue, setInputValue] =  useState([]);
 
-const handleChangeSearch = (event) => {
-  setInputValue(event.target.value)
-  consoel.log(event.target.value)
-}
+// const handleChangeSearch = (event) => {
+//   setInputValue(event.target.value)
+//   consoel.log(event.target.value)
+// }
 
-  // useEffect(() => {
-  //   getArtist()
-  //   .then(response => {      
-  //     const avatars = response.artists
-  //     setArtist(avatars);
-  //   })
-  // }, [])
+  useEffect(() => {
+    getArtist()
+    .then(response => {      
+      const avatars = response.artists
+      setSearchArtist(avatars);
+    })
+  }, [])
 
 
 export default function searchartist() {  
@@ -32,9 +32,9 @@ export default function searchartist() {
         <Nav />
         <div className='mt-20 md:mt-44 mx-10 sm:mx-44 md:mx-72'>
             <SearchByArtist 
-              onChange={handleChangeSearch}
-              onClick={onClick}
-              value={value}
+              // onChange={handleChangeSearch}
+              // onClick={onClick}
+              // value={value}
             />
         </div>
         <OpacityCard className='bg-[#302f56] mx-6 md:mx-20 my-6 md:my-10'>
