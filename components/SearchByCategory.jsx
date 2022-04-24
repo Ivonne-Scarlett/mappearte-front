b4cb5c612027390cea9419431b649a4d9b94551a
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-23 23:46:24 -0500

    Creando componente de busqueda por categoria

--- components/SearchByCategory.jsx
+++ components/SearchByCategory.jsx
@@ -20,58 +20,58 @@ export default function SelectCategories(){
     )}
     >
       <select 
-      name='categories' 
-      id='category' 
-      className={classNames(
-        'w-screen bg-white h-10',
-        'rounded-l-lg pl-4'
-      )}
-      onChange={handleSelect}
+        name='categories' 
+        id='category' 
+        className={classNames(
+          'w-screen bg-white h-10',
+          'rounded-l-lg pl-4'
+        )}
+        onChange={handleSelect}
       >
         <option 
-        value='category' 
-        className={classNames(
-          'bg-white',
-        )}
+          value='category' 
+          className={classNames(
+            'bg-white',
+          )}
         >
           -- Categorias --
         </option>
         <option 
-        value='mural' 
-        className={classNames(
-          'bg-white',
-        )}
+          value='mural' 
+          className={classNames(
+            'bg-white',
+          )}
         >
           Murales
         </option>
         <option 
-        value='graffiti' 
-        className={classNames(
-          'bg-white',
-        )}
+          value='graffiti' 
+          className={classNames(
+            'bg-white',
+          )}
         >
           Graffiti
         </option>
         <option 
-        value='sticker' 
-        className={classNames(
-          'bg-white',
-        )}
+          value='sticker' 
+          className={classNames(
+            'bg-white',
+          )}
         >
           Stickers
         </option>
       </select>
       <button className={classNames(
-      'hover:bg-white/5 backdrop-blur-md',
-      'bg-orangeP font-bold uppercase',
-      'border-orange-200  border-t border-b border-r',
-      'text-zinc-50 h-10',
-      'px-8  rounded-r-lg',
-      'transition duration-500 ease-in-out delay-75',
-      )}
-      onClick={sayHi}
+        'hover:bg-white/5 backdrop-blur-md',
+        'bg-orangeP font-bold uppercase',
+        'border-orange-200  border-t border-b border-r',
+        'text-zinc-50 h-10',
+        'px-8  rounded-r-lg',
+        'transition duration-500 ease-in-out delay-75',
+        )}
+        onClick={sayHi}
       >
-      Buscar
+        Buscar
       </button>
     </div>
   )
---
+++ pages/searchbycategory.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { getArtist } from '../lib/api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Nav from '../components/Nav';
+import AvatarImg from '../components/AvatarImg';
+import SearchByArtist from '../components/SearchByArtist';
+import SearchByCategory from '../components/SearchByCategory';
+import OpacityCard from '../components/OpacityCard';
+import Footer from '../components/Footer';
+
+
+export default function searchbycategory() {
+  const messageFail = () => {
+    toast.warn('Artista no encontrado', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });  
+  }
+
+  const [allArtist, setAllArtist] = useState([]);
+  useEffect(() => {
+    getArtist()
+      .then(response => {
+        const avatars = response.artists
+        setAllArtist(avatars);
+      })
+  }, [])
+
+  const [inputValue, setInputValue]= useState([]);
+  const [filtered, setFiltered] = useState(null);
+
+  const handlerChangeInput= (event) => {
+    const valueInput = event.target.value
+    const datavalueInput = valueInput.toLowerCase()
+    setInputValue(datavalueInput)
+    const filterResult = allArtist.filter( artistFilter => {
+      const valueName= artistFilter.artist
+      //dataValueInput = inputValue.toLowerCase()
+      return valueName.toLowerCase().includes(inputValue)
+    })   
+    setFiltered(filterResult)
+  
+    !valueInput ? setFiltered(null) : setFiltered(filterResult)
+  }
+
+  return (
+    <div>
+      <Nav />
+      <div className='mt-20 md:mt-44 mx-10 sm:mx-44 md:mx-72'>
+        <SearchByArtist
+          value={inputValue}
+          onChange={handlerChangeInput}       
+        />
+        <SearchByCategory />
+      </div>
+      <OpacityCard className='bg-[#20184b] mx-6 md:mx-20 my-6 md:my-10'>
+        { filtered && <AvatarImg artists={filtered} />}
+        { !filtered && <AvatarImg artists={allArtist} /> }
+      </OpacityCard>
+      <Footer />
+    </div>
+  )
+}
