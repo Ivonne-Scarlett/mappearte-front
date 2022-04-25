20477a0582f6d66878c0ce0d34715e0a2a2fdc77
Ivonne <ivonne.scarlett01@gmail.com>
2022-04-24 22:01:58 -0500

    Limpiando el código de console.log y agregando alertas tostify

--- pages/searchbycategory.js
+++ pages/searchbycategory.js
@@ -11,18 +11,6 @@ import Footer from '../components/Footer';
 
 
 export default function searchbycategory() {
-  const messageFail = () => {
-    toast.warn('Artista no encontrado', {
-      position: "top-right",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });  
-  }
-
   const [allArtist, setAllArtist] = useState([]);
   const[resultFilterCategory, setResultFilterCategory] = useState([]);
   const [categorySelect, setCategorySelect]= useState('')
@@ -37,47 +25,49 @@ export default function searchbycategory() {
 
   const handleSelect = event => {
     setCategorySelect(event.target.value)
-    console.log(categorySelect)
+  }
+
+  const messageOk = () => {
+    toast.success('Artistas encontrados.', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   }
 
   const handlerButton = () => {
-    console.log('EL boton funciona')
-    console.log('la categoria seleccioanda es',categorySelect)
     if(categorySelect==='mural'){
-      console.log('Mostrar sólo datos de mural')
-      const filterResult = allArtist.filter( artistFilter => {
-        console.log(artistFilter)
-        if (artistFilter.isMural === true){
-          return artistFilter
-        }      
-      })   
-      setResultFilterCategory(filterResult)      
+      const filterResult = allArtist.filter( artistFilter => artistFilter.isMural === true ) 
+      messageOk() 
+      setResultFilterCategory(filterResult)     
     } else if (categorySelect==='graffiti') {
-      console.log('Mostrar sólo datos de grafiti')
-      const filterResult = allArtist.filter( artistFilter => {
-        console.log(artistFilter)
-        if (artistFilter.isGraffiti === true){
-          return artistFilter
-        }      
-      })
+      const filterResult = allArtist.filter( artistFilter => artistFilter.isGraffiti === true ) 
+      messageOk()  
       setResultFilterCategory(filterResult)    
     } else if (categorySelect==='sticker') {
-      console.log('Mostrar sólo datos de sticker')
-      const filterResult = allArtist.filter( artistFilter => {
-        console.log(artistFilter)
-        if (artistFilter.isSticker === true){
-          return artistFilter
-        }      
-      })
+      const filterResult = allArtist.filter( artistFilter => artistFilter.isSticker === true ) 
+      messageOk() 
       setResultFilterCategory(filterResult)   
     } else {
-      console.log('Mostrar todos los artistas')
+      messageOk() 
       setResultFilterCategory(allArtist)
-      console.log(allArtist)
     }
   }
-
-
 
   return (
     <div>
@@ -89,7 +79,6 @@ export default function searchbycategory() {
         />
       </div>
       <OpacityCard className='bg-[#20184b] mx-6 md:mx-20 my-6 md:my-10'>
-        {/* <AvatarImg artists={resultFilterCategory} /> */}
         { resultFilterCategory && <AvatarImg artists={resultFilterCategory} /> }
         { !categorySelect && <AvatarImg artists={allArtist} /> }
       </OpacityCard>
