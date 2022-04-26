4c176d9d31e19fedddb5d10ef5c34ead69f3a3fa
Sara Reveles <elizabethrc@gmail.com>
2022-04-26 14:50:54 -0400

    Vista de upload lista, s3 integrado

--- pages/upload.js
+++ pages/upload.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import Nav from '../components/Nav';
-import UploadImages from '../components/UserProfile'
+import { React, useState, useEffect } from 'react';
 import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import Uppy from "@uppy/core";
+import Transloadit from "@uppy/transloadit";
+
+import { getUserById } from "../lib/api";
+import { getArtistById } from "../lib/api";
+
+import UploadImages from '../components/UserProfile'
 import {
+  Nav,
   Input,
   InputFile,
   OpacityCard,
@@ -10,7 +17,73 @@ import {
   Category
 } from '../components'
 
-export default function Test() {
+export default function upload() {
+  const [role, setRole] = useState()
+  const [artist, setArtist] = useState();
+  const [user, setUser] = useState();
+  const router = useRouter();
+  const [uppy, setUppy] = useState();
+  const [isUploadingFile, setIsUploadingFile] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+  
+  const onCompleteUploadFiles = (assembly) => {
+    const image = assembly.results?.compress_image[0].ssl_url;
+    setImageUrl(image);
+    setIsUploadingFile(false);
+  };
+  
+  const onFileInputChange = (event) => {
+    setIsUploadingFile(true);
+    const file = Array.from(event.target.files)[0] || null;
+    if (file) {
+      uppy.reset();
+      uppy.addFile({
+        name: file.name,
+        type: file.type,
+        data: file,
+      });
+      uppy.upload();
+    }
+  };
+  
+  useEffect(() => {
+    const uppyInstance = new Uppy({
+      restrictions: {
+        maxNumberOfFiles: 1,
+      },
+    })
+    .use(Transloadit, {
+      params: {
+        auth: { key: process.env.NEXT_PUBLIC_TRANSLOADIT_AUTH_KEY },
+        template_id: process.env.NEXT_PUBLIC_TRANSLOADIT_TEMPLATE_ID,
+      },
+      waitForEncoding: true,
+    })
+    setUppy(uppyInstance);
+  }, []);
+  
+  console.log(imageUrl)
+  useEffect( () => {
+    const id = localStorage.getItem("id");
+    const role = localStorage.getItem("role");
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.push("/Login")
+    } 
+      setRole(role);
+      if (role === 'artist') {
+        getArtistById(id).then( ({artists}) => {
+          setArtist(artists);
+        }, {})
+      } else if (role === "user"){
+        getUserById(id).then( ({users})  =>{
+        setUser(users);
+        }, {})
+      }
+    }, []
+  )
+
   return (
     <>
       <Nav/>
@@ -21,16 +94,30 @@ export default function Test() {
     )}>
         <OpacityCard className={classNames(
           "w-5/6 md:w-4/5 items-center my-8 ",
-          "grid gird-cols-1 md:grid-cols-2 gap-4 mt-20 mb-20 md:mt-40 md:mb-20"
+          "grid gird-cols-1 md:grid-cols-2 gap-4", 
+          "mt-20 mb-20 md:mt-32 md:mb-10"
         )}>
-          <div className='md:col-span-1'>
+          <div className="md:col-span-2 md:row-span-1 justify-center">
+            <img 
+              src="../icons/sube-tu-arte.png" 
+              alt="Sube tu arte" 
+              className="mx-auto" 
+            />
+          </div>
+          <div 
+          className='md:col-span-1'
+          >
             <Input 
-            label='Nombre artístico:'
+            label='Nombre del artísta:'
             placeholder='ejemplo: MexiArt'
             name='artist'
-            className='mb-4 mt-0'
             />
-            <div className="flex flex-row mt-1 w-full my-2">
+
+            <label>
+              Categoria:
+            </label>
+            <div 
+            className="flex flex-row mt-1 w-full my-2">
               <Category 
                 className='rounded-l-lg my-4'
                 name='Sticker'
@@ -47,25 +134,35 @@ export default function Test() {
               />
             </div>
             <InputFile
-            className='my-4'
+            id='file'
+            name='file'
+            type='file'
+            accept='image/png, image/jpeg'
+            onChange={onFileInputChange}
             />
             <Input 
-            label='Direccion:'
-            placeholder='ejemplo: Col. #'
+            label='Dirección:'
+            placeholder='ejemplo: Calle A #1, Del. B'
             name='address'
             className='my-4'
             />
           </div>
           <div 
           className='md:col-span-1 align-center '>
-            <div className='bg-white w-100 text-black h-56'>Mapa</div>
+            <div className='bg-white w-100 text-black h-72'>
+              Mapa
+            </div>
+          </div>
+          <div 
+          className="flex justify-center items-center md:col-span-2 md:row-span-1">
             <ButtonSend 
-            className='mt-4' 
-            type='submit' 
-            text='Registrar'
-            bgColor='Orange'
-            borderColor='Orange'
-            className='mt-6'/>
+              className='mt-4' 
+              type='submit' 
+              text='Guardar'
+              bgColor='Orange'
+              borderColor='Orange'
+              className='w-2/3 md:w-1/3'
+            />
           </div>
         </OpacityCard>
       </div>
