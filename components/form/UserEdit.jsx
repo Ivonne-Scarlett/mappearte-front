0b573c340d557380e5026ece2f003c68216fab30
Rosa Ortega <rose@Rosas-MacBook-Pro.local>
2022-05-13 04:18:28 -0500

    Testing footer in short content

--- components/form/UserEdit.jsx
+++ components/form/UserEdit.jsx
@@ -11,6 +11,7 @@ import { getUserById, getArtistById, patchUser } from "../../lib/api";
 import {
   Nav, 
   DarkBlueCard,
+  OpacityCard,
   InputFile,
   Category, 
   ButtonSend,
@@ -158,95 +159,97 @@ export default function UserEdit () {
   const defaultImage = "/icons/noavatar.png";
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col justify-between"
-    >
-      <Nav/>
-      <div
-      className={classNames(
-        "bg-artist bg-cover bg-center bg-no-repeat",
-        "flex justify-center items-center",
-        "h-96",
-        "w-full",
-        "px-6 md:px-20",
-        "mt-24"
-      )}
-    >
-    </div>
-
-      <DarkBlueCard>
-      <div 
-      className="flex justify-center mt-2 mb-8">
-        <h3 
-        className="font-bold text-xl text-gray-400 mt-2 font-Mali">
-          { user?.user || "userName" }
-        </h3>
-      </div>
-        <div className="flex justify-center mt-3">
-          <img
-            src={ user?.imgUser || defaultImage}
-            alt="Icono de perfil de usuario"
-            className={classNames(
-              "bg-backgroundP object-cover", 
-              "rounded-full border-2 border-solid border-orangeP",
-              "w-24 h-24 justify-center",
-              "md:w-32 md:h-32 mb-8",
-              "lg:w-40 lg:h-40",
-              "2xl:w-60 2xl:h-60"
-            )}
-          />
+    <>
+      <Nav />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-between lg:min-h-[calc(100vh-120px)]"
+      >
+        <div
+          className={classNames(
+            "bg-artist bg-cover bg-center bg-no-repeat",
+            "flex justify-center items-center",
+            "h-96",
+            "w-full",
+            "px-6 md:px-20",
+            "mt-24"
+          )}
+        >
         </div>
-        
-        <div className="flex flex-row mx-auto mb-6 md:w-2/3">
-          <div className="w-full">
-            <InputFile
-            id='file'
-            name='file'
-            type='file'
-            accept='image/png, image/jpeg'
-            onChange={onFileInputChange}
-            register= {register("imgUser")} 
+        <DarkBlueCard>
+          <div
+            className="flex justify-center mt-2 mb-8">
+            <h3
+              className="font-bold text-xl text-gray-400 mt-2 font-Mali">
+              {user?.user || "userName"}
+            </h3>
+          </div>
+          <div className="flex justify-center mt-3">
+            <img
+              src={user?.imgUser || defaultImage}
+              alt="Icono de perfil de usuario"
+              className={classNames(
+                "bg-backgroundP object-cover",
+                "rounded-full border-2 border-solid border-orangeP",
+                "w-24 h-24 justify-center",
+                "md:w-32 md:h-32 mb-8",
+                "lg:w-40 lg:h-40",
+                "2xl:w-60 2xl:h-60"
+              )}
             />
           </div>
-        </div>
-        
-        <div className="flex flex-col mx-auto mt-6 md:w-2/3">
-        <label className="my-2">
-          Categoria:
-        </label>
-        <div className="flex flex-row mx-auto w-full ">
-          <Category
-            className="rounded-l-lg"
-            name="Sticker"
-            value={watch("isSticker")}
-            register={register("isSticker")}
-          />
-          <Category
-            name="Mural"
-            value={watch("isMural")}
-            register={register("isMural")}
-          />
-          <Category
-            className="rounded-r-md"
-            name="Grafitti"
-            value={watch("isGraffitti")}
-            register={register("isGraffitti")}
-          />
-        </div>
-        </div>
-        <div className='flex ' >
-          <ButtonSend
-          className='mx-auto mt-12 md:w-1/3' 
-          type='submit' 
-          text='Guardar'
-          bgColor='Orange'
-          borderColor='Orange'
-          />
-        </div>
-        
-      </DarkBlueCard>
-      <Footer/>
-    </form>
-  )
+
+          <div className="flex flex-row mx-auto mb-6 md:w-2/3">
+            <div className="w-full">
+              <InputFile
+                id='file'
+                name='file'
+                type='file'
+                accept='image/png, image/jpeg'
+                onChange={onFileInputChange}
+                register={register("imgUser")}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col mx-auto mt-6 md:w-2/3">
+            <label className="my-2">
+              Categoria:
+            </label>
+            <div className="flex flex-row mx-auto w-full ">
+              <Category
+                className="rounded-l-lg"
+                name="Sticker"
+                value={watch("isSticker")}
+                register={register("isSticker")}
+              />
+              <Category
+                name="Mural"
+                value={watch("isMural")}
+                register={register("isMural")}
+              />
+              <Category
+                className="rounded-r-md"
+                name="Grafitti"
+                value={watch("isGraffitti")}
+                register={register("isGraffitti")}
+              />
+            </div>
+          </div>
+          <div className='flex ' >
+            <ButtonSend
+              className='mx-auto mt-12 md:w-1/3'
+              type='submit'
+              text='Guardar'
+              bgColor='Orange'
+              borderColor='Orange'
+            />
+          </div>
+
+        </DarkBlueCard>
+      </form>
+      <Footer />
+
+    </>
+      )
 }
